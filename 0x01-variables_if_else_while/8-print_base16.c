22bcec505cbe567096bec4d2eccd1e2046116654
BlueBelle22 <veelit47@gmail.com>
2023-02-17 19:57:06 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - Entry point
  *Return: 0
@@ -5,22 +6,16 @@
 
 int main(void)
 {
-	char c;
+	char ch;
 
-	int d;
-
-	c = 'a';
-	d = 0;
-	while
-		(d < 10) {
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c <= 'f') {
-			putchar(c);
-			c++;
-		}
-	putchar('\n');
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
 	return (0);
 }
