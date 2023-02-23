5faee46c6d0c47e4b28e5c0e7f26956116fcc519
BlueBelle22 <veelit47@gmail.com>
2023-02-22 19:08:10 -0800

    0 task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description for header
+ * Return: 0
+ */
+
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX /
+	return (0);
+}
